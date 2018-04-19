b9add8c0bfd54bc772e41ea66f9e5123b5e27007
RudiAndre <dahle05@gmail.com>
2018-04-19 18:12:48 +0200

    Search patient/doctor

--- GroupFour_DoctorsOffice/widget.h
+++ GroupFour_DoctorsOffice/widget.h
@@ -26,6 +26,10 @@ class Widget : public QWidget
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
+    void filter_doctors(QString filter_string);
+    void filter_patients(QString filter_string);
+    void hide_all_doctors();
+    void hide_all_patients();
 
 
 private slots:
@@ -36,7 +40,9 @@ private slots:
     void on_button_AddDoctor_clicked();
     void refresh_lists();
 
-    void on_filterLineEdit_textChanged(const QString &arg1);
+    void on_filter_doctor_edit_textChanged(const QString &arg1);
+
+    void on_filter_patient_edit_textChanged(const QString &arg1);
 
 private:
     Ui::Widget *ui;
