402891a38c745235f1066f6b8c3b825c8bbd436e
pontusskold <pontusskold@PONTUSSKLD1C1D>
2018-04-24 11:37:01 +0200

    widget kommentarer

--- GroupFour_DoctorsOffice/widget.h
+++ GroupFour_DoctorsOffice/widget.h
@@ -64,33 +64,90 @@ public:
 
 
 private slots:
-  
+
+    /**
+     * @brief on_button_SelectDoctor_clicked handterer knappetrykk ved vald doctor objekt.
+     * Denne fører til doctorpage dialogen
+     */
     void on_button_SelectDoctor_clicked();
 
+    /**
+     * @brief on_listWidget_Doctors_itemClicked handterer trykk på objekt i doctor listen.
+     * Denne gir informasjon om hvilken doktor som skal bli sendt videre.
+     */
     void on_listWidget_Doctors_itemClicked();
 
+    /**
+     * @brief on_button_SelectPatient_clicked handterer knappetrykk ved vald patient objekt.
+     * Denne fører videre til patientpage dialogen.
+     */
     void on_button_SelectPatient_clicked();
 
+    /**
+     * @brief on_button_AddPatient_clicked leder til AddDoctorDialog som gir mulighet til å
+     * legge til en ny pasient i systemet.
+     */
     void on_button_AddPatient_clicked();
 
+    /**
+     * @brief on_button_AddDoctor_clicked leder til AddPatientDialog som gir mulighet til å
+     * legge til en ny lege i systemet.
+     */
     void on_button_AddDoctor_clicked();
 
+    /**
+     * @brief refresh_lists oppdaterer listen over lege og pasienter i systemet
+     */
     void refresh_lists();
 
+    /**
+     * @brief filter_appointments filtrer legetimer basert på søk
+     * @param filter_string
+     */
     void filter_appointments(const QString& filter_string);
 
+    /**
+     * @brief on_filter_doctor_edit_textChanged filtrer listen over leger basert på søk
+     * @param arg1
+     */
     void on_filter_doctor_edit_textChanged(const QString &arg1);
 
+    /**
+     * @brief on_filter_patient_edit_textChanged filtrer listen over pasient basert på søk
+     * @param arg1
+     */
     void on_filter_patient_edit_textChanged(const QString &arg1);
 
+    /**
+     * @brief on_listWidget_Patients_itemClicked handterer knappetrykk til pasient listen.
+     * Den valgte pasienten kan da bli videresendt til andre metoder
+     */
     void on_listWidget_Patients_itemClicked();
 
+    /**
+     * @brief on_listWidget_Appointments_itemClicked handterer knappetrykk til legetime listen.
+     * Den valgte legetimen kan da bli videresendt til andre metoder
+     */
     void on_listWidget_Appointments_itemClicked();
 
+    /**
+     * @brief on_button_SelectAppointment_clicked leder til dialogen AddAppointmentNotes.
+     * Der kan notater til legetimen legges inn.
+     */
     void on_button_SelectAppointment_clicked();
 
+    /**
+     * @brief list_doctor_time er listen over legetimer. Listen endrer sig basert på
+     * hvilken lege som er valgt.
+     * @param doctorId
+     */
     void list_doctor_time(int doctorId);
 
+    /**
+     * @brief on_comboBox_activated gjør et kall på filter appointments da søk
+     * kriterier blir endret
+     * @param arg1
+     */
     void on_comboBox_activated(const QString &arg1);
 
 private:
